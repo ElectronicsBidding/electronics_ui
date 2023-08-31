f24845b77431407f2829599ae2f7338ced811241
Bete77 <betemariamabenet8468@gmail.com>
2023-08-31 17:25:21 +0300

    Implemented the landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hero from '../components/home/Hero'
 
 const Home = () => {
   return (
-    <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>Home</div>
+
+    <>
+      <Hero />
+    </>
+
   )
 }
 
