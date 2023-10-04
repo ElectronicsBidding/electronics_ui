432c97821c8f511af80dad20578178b39f602420
Bete77 <betemariamabenet8468@gmail.com>
2023-10-04 15:37:28 +0300

    Fetch products from API

--- src/components/trending/Products.jsx
+++ src/components/trending/Products.jsx
@@ -3,39 +3,43 @@ import './products.css';
 
 const Products = () => {
   const [productData, setProductData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch('http://127.0.0.1:3000/products')
       .then((response) => response.json())
       .then((data) => {
-        setProductData(data);
+        setProductData(data["data"]);
+        setLoading(false);
       })
       .catch((error) => {
         console.error('Error fetching product data:', error);
       });
-      console.log(productData);
   }, []);
-
 
   return (
     <section className="product__sec">
       <div className="products">
-        {productData.map((product, index) => (
-          <div className="product__card" key={index}>
-            <div>
-              <img src={product.image} alt="" className="product__img"/>
-            </div>
-            <div className="product__content">
-              <div className="product__title">{product.name}</div>
-              <div className="product__category">{product.category_id}</div>
-            </div>
+        {productData.length > 0 ? (
+          productData.map((product, index) => (
+            <div className="product__card" key={index}>
+              <div>
+                <img src={product.image} alt="" className="product__img" />
+              </div>
+              <div className="product__content">
+                <div className="product__title">{product.name}</div>
+                <div className="product__category">{product.category_id}</div>
+              </div>
 
-            <div className="product__box">
-              <div className="product__price">SP: ${product.starting_price}</div>
-              <a className="product__btn" href={product.buttonPath}>Bid Now</a>
+              <div className="product__box">
+                <div className="product__price">SP: ${product.starting_price}</div>
+                <a className="product__btn" href={product.buttonPath}>Bid Now</a>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
     </section>
   );
